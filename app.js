c992387fcd73496cc487d782bd013de4878f8d56
Odilbekov O`lmasbek <oolmasbek754@gmail.com>
2026-04-04 14:37:14 +0500

    Add initial app.js with variable declarations

---
+++ app.js
@@ -0,0 +1,16 @@
+let text1 = "What a very ";
+text1 += "nice day";
+
+let x = 5 + 5;
+let y = "5" + 5;
+let z = "Hello" + 5;
+
+let x = 10;
+x += 5;
+
+let x = 5;
+let result = x > 8;
+
+let text1 = "A";
+let text2 = "B";
+let result = text1 < text2;
